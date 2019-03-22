c68dba843b3b995e6f8744c1eb69e50e045d37cc
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-03-21 23:24:58 -0700

    personalized default graph as far as styling. Need to Now make graph reflect true data

--- src/components/Dashboard/PriceChart.js
+++ src/components/Dashboard/PriceChart.js
@@ -3,7 +3,9 @@ import HighchartsConfig from './HighchartsConfig';
 import { Tile } from '../../styles/Tile';
 import { AppContext } from '../../AppProvider';
 import ReactHighCharts from 'react-highcharts';
+import HighchartsTheme from './HighchartsTheme.js';
 
+ReactHighCharts.Highcharts.setOptions(HighchartsTheme); //ReactHighcharts (to activate livrary) .Highcharts to get root highchart object. .setOptions(highchartstheme) to apply the custome theme
 export default function() {
   return (
     <AppContext.Consumer>
